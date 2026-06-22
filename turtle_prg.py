a605f5a9b2adcff547b5722770fd7004c554c9e5
Techsrijan <aswanibtech@gmail.com>
2026-06-22 10:44:57 +0530

    turtle intro

---
+++ turtle_prg.py
@@ -0,0 +1,6 @@
+from turtle import *
+
+circle(100,steps=10)
+circle(-100,extent=90)
+write("India",font=("Comic Sans MS",15,"bold"))
+done()
---
+++ turtlestar.py
@@ -0,0 +1,10 @@
+from turtle import *
+sc=Screen()
+sc.setup(800,400)
+l=['red','green','blue','orange','yellow']
+for i in range(5):
+    pencolor(l[i])
+    fd(200)
+    left(144)
+
+done()
