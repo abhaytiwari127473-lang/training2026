34c97a4d281e85b1e0f473bc6c097a1a57e564d0
Sagar Paswan <www.sagarpaswan719@gmail.com>
2026-06-24 10:58:57 +0530

    Sagar

--- Sagar.py
+++ Sagar.py
@@ -2,8 +2,8 @@ from turtle import*
 t=Turtle()
 t.speed(0)
 color = ["red", "green", "blue", "yellow", "cyan", "magenta"]
-for i in range(75):
+for i in range(500):
         t.pencolor(color[i%5])
         t.forward(i*10)
-        t.left(144)
+        t.left(145)
 mainloop()
